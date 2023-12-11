282028a4725f6a427768d6323efc6ea7fde10b43
unknown <ruslanchik_nadyrov@mail.ru>
2023-12-11 13:12:28 +0600

    add dist folder

---
+++ dist/assets/useToogleLang-3c503cd3.js
@@ -0,0 +1,1 @@
+import{h as o}from"./index-b9744a89.js";const t=()=>{const{t:a,locale:e}=o({useScope:"global"});return{t:a,updateLocale:()=>{e.value==="kz"?e.value="ru":e.value="kz"}}};export{t as u};
