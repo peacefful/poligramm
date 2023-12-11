413c1c07086bbce260e9544226119dac893d79f9
unknown <ruslanchik_nadyrov@mail.ru>
2023-12-11 13:11:36 +0600

    add dist folder

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,7 +1,7 @@
 import kz from "./kz.json"
 import ru from "./ru.json"
 
-export const defaultLocale = localStorage.getItem("lang")
+export const defaultLocale = localStorage.getItem("lang") || undefined
 
 export const languages = {
 	kz,
