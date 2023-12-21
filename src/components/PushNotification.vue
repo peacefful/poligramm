7f0647d405504b65dd248bb154a122d8f34fa107
unknown <ruslanchik_nadyrov@mail.ru>
2023-12-21 17:42:54 +0600

    notification component

---
+++ src/components/PushNotification.vue
@@ -0,0 +1,56 @@
+<template>
+	<transition>
+		<div v-if="isInviteRoom && showChats" class="fixed bottom-0 right-0 w-[380px] bg-blue-600 p-4 rounded-xl">
+			<p class="mb-2">Вас пригласили в чат</p>
+			<CustomButton title="Войти" @click.prevent="$emit('enterChat')" />
+			<CustomButton @click.prevent="closeModal()" class="bg-red-600 hover:bg-red-700 ml-3" title="Закрыть" />
+		</div>
+	</transition>
+</template>
+
+<script setup lang="ts">
+import CustomButton from "@/components/ui/UICustomButton.vue"
+import socket from "@/utils/socket"
+import { reactive, ref } from "vue";
+
+defineProps<{
+	showChats: boolean
+}>()
+
+defineEmits(['enterChat'])
+
+interface IInviteRoom {
+	nameRoom: string
+	uuidRoom: string
+}
+
+const inviteRoom: IInviteRoom = reactive({
+	nameRoom: "",
+	uuidRoom: ""
+})
+
+let isInviteRoom = ref<boolean>(false)
+const closeModal = () => isInviteRoom.value = false
+
+socket.on('messageInvite', (nameRoom, uuidRoom) => {
+	inviteRoom.nameRoom = nameRoom
+	inviteRoom.uuidRoom = uuidRoom
+
+	if (inviteRoom.nameRoom && inviteRoom.uuidRoom) {
+		isInviteRoom.value = true
+		setTimeout(() => isInviteRoom.value = false, 5000);
+	}
+})
+</script>
+
+<style scoped>
+.v-enter-active,
+.v-leave-active {
+	transition: opacity 0.2s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+	opacity: 0;
+}
+</style>
--- src/views/home-view/HomeView.vue
+++ src/views/home-view/HomeView.vue
@@ -24,15 +24,16 @@
 			</section>
 		</article>
 		<article v-else class="w-full flex flex-col justify-between">
-			<ChatContainer @close-chat="closeChat" :uuid="uuid" :name="room" />
+			<ChatContainer @close-chat="closeChat()" :uuid="uuid" :name="room" />
 		</article>
-		<transition>
-			<div v-if="isInviteRoom" class="fixed bottom-0 right-0 w-[380px] bg-blue-600 p-4 rounded-xl">
+		<PushNotification :show-chats="showChats" @enter-chat="enterChat(inviteRoom.nameRoom, inviteRoom.uuidRoom)" />
+		<!-- <transition>
+			<div v-if="isInviteRoom && showChats" class="fixed bottom-0 right-0 w-[380px] bg-blue-600 p-4 rounded-xl">
 				<p class="mb-2">Вас пригласили в чат</p>
-				<CustomButton title="Войти" @click.prevent="enterChat(inviteRoom.nameRoom, inviteRoom.uuidRoom)" />
-				<CustomButton @click.prevent="closeModal()" class="bg-red-600 hover:bg-red-700 ml-3" title="Закрыть" />
+				<CustomButton title="Войти" @click.prevent="enterChat(inviteRoom.nameRoom, inviteRoom.uuidRoom, isInviteRoom)" />
+				<CustomButton @click.prevent="closeNotification" class="bg-red-600 hover:bg-red-700 ml-3" title="Закрыть" />
 			</div>
-		</transition>
+		</transition> -->
 	</main>
 	<div v-else class="text-5xl flex flex-col justify-center items-center">
 		<router-link to="/" class="mt-14">Войдите</router-link>
@@ -50,8 +51,9 @@ import { ref, reactive } from 'vue';
 import { useI18n } from "vue-i18n"
 import socket from "@/utils/socket"
 import ChatContainer from "@/components/ChatContainer.vue"
-import { useEnterChat } from "@/hooks/useEnterChat";
+import { useEnterChat, useEnterChatFromNotification } from "@/hooks/useEnterChat";
 import { storage } from "@/utils/storage"
+import PushNotification from "@/components/PushNotification.vue"
 
 interface IInviteRoom {
 	nameRoom: string
@@ -64,39 +66,29 @@ const { t } = useI18n({ useScope: 'global' })
 const toogleMessenger = ref<boolean>(false)
 const toogleInvitesInChats = ref<boolean>(false)
 
-const { enterChat, closeChat, showChats, uuid, room } = useEnterChat()
-
 const inviteRoom: IInviteRoom = reactive({
 	nameRoom: "",
 	uuidRoom: ""
 })
 
-let isInviteRoom = ref<boolean>(false)
-const closeModal = () => isInviteRoom.value = false
-
 socket.emit('personalInvite', storage.getData("uuid"));
+const { enterChat, closeChat, showChats, uuid, room } = useEnterChat()
 
-socket.on('messageInvite', (nameRoom, uuidRoom) => {
-	inviteRoom.nameRoom = nameRoom
-	inviteRoom.uuidRoom = uuidRoom
+// let isInviteRoom = ref<boolean>(false)
+// const closeNotification = () => isInviteRoom.value = false
 
-	if (inviteRoom.nameRoom && inviteRoom.uuidRoom) {
-		isInviteRoom.value = true
-		setTimeout(() => isInviteRoom.value = false, 5000);
-	}
-})
+
+// socket.on('messageInvite', (nameRoom, uuidRoom) => {
+// 	inviteRoom.nameRoom = nameRoom
+// 	inviteRoom.uuidRoom = uuidRoom
+
+// 	if (inviteRoom.nameRoom && inviteRoom.uuidRoom) {
+// 		isInviteRoom.value = true
+// 		setTimeout(() => isInviteRoom.value = false, 5000);
+// 	}
+// })
 </script>
 
 <style scoped>
 @import "./styles.module.scss";
-
-.v-enter-active,
-.v-leave-active {
-	transition: opacity 0.2s ease;
-}
-
-.v-enter-from,
-.v-leave-to {
-	opacity: 0;
-}
 </style>
