84de47aa410bab27e376ccb27e2c622d2bc5b721
unknown <ruslanchik_nadyrov@mail.ru>
2023-12-29 01:20:49 +0600

    update dist

---
+++ dist/assets/useToogleLang-17d38694.js
@@ -0,0 +1,1 @@
+import{h as o}from"./index-d869a618.js";const t=()=>{const{t:a,locale:e}=o({useScope:"global"});return{t:a,updateLocale:()=>{e.value==="kz"?e.value="ru":e.value="kz"}}};export{t as u};
