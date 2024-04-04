894b3ce968673016194cf9e856ab930c0e42e357
unknown <ruslanchik_nadyrov@mail.ru>
2024-04-04 19:11:18 +0600

    Updates

---
+++ assets/toogleLang-47ea7f61.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-1118abf8.js";const l=()=>{const{t:a,locale:e}=o({useScope:"global"});return{t:a,updateLocale:()=>e.value==="kz"?e.value="ru":e.value="kz"}};export{l as t};
