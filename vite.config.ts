29df3af0ab15f29f71a6e50033c0e4eaa9450e5e
unknown <ruslanchik_nadyrov@mail.ru>
2024-12-18 20:06:16 +0600

    build: dele gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,9 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
@@ -14,5 +16,5 @@ export default defineConfig({
   define: {
     __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'true'
   },
-  base: '/poligramm'
+  base: '/poligramm/'
 })
