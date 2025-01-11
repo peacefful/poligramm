6f08cb96560d242abe200c2b27b8f90004ebfd27
peacefull <peacfull333@gmail.com>
2025-01-11 17:50:46 +0500

    fix: check current lang from cookies

--- src/app/i18n/i18n.config.ts
+++ src/app/i18n/i18n.config.ts
@@ -3,7 +3,7 @@ import ru from "./ru.json";
 
 export default defineI18nConfig(() => ({
   legacy: false,
-  locale: "ru",
+  locale: useCookie("lang").value || 'ru',
   fallbackLocale: "ru",
   messages: {kz, ru},
 }));
