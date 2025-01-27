de77a32ed8dbc14cfb48548f7e02f2eac6cca299
peacefull <peacfull333@gmail.com>
2025-01-27 21:20:49 +0500

    fix: delete chat

---
+++ src/entities/chat/ui/skeletonChatCard/SkeletonChatCard.vue
@@ -0,0 +1,16 @@
+<template>
+  <section class="p-3 flex justify-between items-start">
+    <div class="flex items-center gap-3">
+      <Skeleton class="rounded-full w-14 h-14" />
+      <div class="flex flex-col gap-4">
+        <Skeleton class="rounded-2xl w-12 h-4" />
+        <Skeleton class="rounded-2xl w-[200px] h-4" />
+      </div>
+    </div>
+    <Skeleton class="rounded-2xl w-11 h-4" />
+  </section>
+</template>
+
+<script setup lang="ts">
+import {Skeleton} from "@/shared/ui/skeleton";
+</script>
---
+++ src/shared/ui/skeleton/Skeleton.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="max-w-sm animate-pulse">
+    <div v-bind="$attrs" class="bg-stone-300 dark:bg-default"></div>
+  </div>
+</template>
